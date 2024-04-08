62392f0aec8c04c83ad6e443177f2e246e53f88a
NeeyC10 <164272813+NeeyC10@users.noreply.github.com>
2024-04-08 19:31:38 -0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,13 +1,20 @@
-CREATE TABLE usuarios (
+CREATE TABLE clientes (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100),
   email VARCHAR(100),
-  nascimento DATE,
-  fone INT,
-  local VARCHAR(100)
+  senha VARCHAR(100),
+  cpf VARCHAR(100)
+  );
+  
+CREATE TABLE filmes (
+id INT AUTO_INCREMENT PRIMARY KEY,
+titulos VARCHAR(100),
+duracao INT,
+status VARCHAR(100)
 );
 
-CREATE TABLE seguidores (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100)
-);
+CREATE TABLE clientes_filmes (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    filme_id INT,
+    cliente_id INT
+    );
